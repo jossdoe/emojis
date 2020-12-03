b08703aa214f9c883c41501dc3905e9da8bab0d3
Joss Doebler <mail@joss-doebler.de>
2020-12-03 17:53:46 +0100

    Add logic for score submit button

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,35 @@
 import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 import useCountdown from 'hooks/useCountdown';
 import { Instructions, PlayButton, ScoreButton } from 'styles/index.styles';
 
 const IndexPage = () => {
+  const router = useRouter();
   const [gameState, setGameState] = useState('index');
+
   const preCountdown = useCountdown(5, gameState === 'countdown', () => {
     setGameState('gameloop');
   });
   const gameCountdown = useCountdown(10, gameState === 'gameloop', () => {
     setGameState('result');
   });
+
   const [gameScore, setGameScore] = useState(0);
   const [currentEmoji, setCurrentEmoji] = useState('🤓');
   const [emojiField, setEmojiField] = useState('');
   const [nameField, setNameField] = useState('');
+  const [isLoadingScore, setIsLoadingScore] = useState(false);
+
+  const handleScoreSubmit = async () => {
+    setIsLoadingScore(true);
+
+    // API Call
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    setIsLoadingScore(false);
+    router.push('/scores');
+  };
 
   if (gameState === 'countdown')
     return (
@@ -50,7 +65,11 @@ const IndexPage = () => {
             onChange={(e) => setNameField(e.currentTarget.value)}
           />
         </div>
-        <button>Add to scoreboard</button>
+        {isLoadingScore ? (
+          'Loading...'
+        ) : (
+          <button onClick={handleScoreSubmit}>Add to scoreboard</button>
+        )}
         <button onClick={() => setGameState('countdown')}>Try again</button>
       </>
     );
