f4c71e2dda1f4565d8ed3641be53a9029cbc610a
Joss Doebler <mail@joss-doebler.de>
2020-12-03 18:33:35 +0100

    Add curcial logic to the game loop

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,46 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 
 import useCountdown from 'hooks/useCountdown';
+import getRandomEmoji from 'utils/getRandomEmoji';
 import Countdown from 'components/Countdown';
 import Gameloop from 'components/Gameloop';
 import Result from 'components/Result';
+
 import { Instructions, PlayButton, ScoreButton } from 'styles/index.styles';
 
 const IndexPage = () => {
   const router = useRouter();
-  const [gameState, setGameState] = useState('index');
 
+  // General game states
+  const [gameState, setGameState] = useState('index');
+  const [gameScore, setGameScore] = useState(0);
+
+  // Countdown states
   const preCountdown = useCountdown(5, gameState === 'countdown', () => {
     setGameState('gameloop');
   });
-  const gameCountdown = useCountdown(10, gameState === 'gameloop', () => {
+  const gameCountdown = useCountdown(20, gameState === 'gameloop', () => {
     setGameState('result');
   });
 
-  const [gameScore, setGameScore] = useState(0);
-  const [currentEmoji, setCurrentEmoji] = useState('🤓');
+  // Emoji states
+  const [currentEmoji, setCurrentEmoji] = useState(getRandomEmoji(null));
   const [emojiField, setEmojiField] = useState('');
+
+  // Set Score to Zero whenever a new round starts
+  useEffect(() => {
+    if (gameState === 'countdown') setGameScore(0);
+  }, [gameState]);
+
+  // Check if user types the correct emoji, award points and reset
+  useEffect(() => {
+    if (emojiField === currentEmoji) {
+      setGameScore(gameScore + 1);
+      setCurrentEmoji(getRandomEmoji(currentEmoji));
+      setEmojiField('');
+    }
+  }, [emojiField]);
 
   if (gameState === 'countdown') return <Countdown count={preCountdown} />;
 
---
+++ utils/getRandomEmoji.js
@@ -0,0 +1,22 @@
+const emojis = [
+  '😀',
+  '😃',
+  '😄',
+  '😁',
+  '😆',
+  '😅',
+  '😂',
+  '🤣',
+  '🥲',
+  '☺️',
+  '😊',
+  '😇',
+];
+
+const getRandomEmoji = (prevEmoji) => {
+  const cleanEmojis = emojis.filter((em) => em !== prevEmoji);
+  const randEmoji = cleanEmojis[Math.floor(Math.random() * cleanEmojis.length)];
+  return randEmoji;
+};
+
+export default getRandomEmoji;
